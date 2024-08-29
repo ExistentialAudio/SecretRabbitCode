80760397a4500a52a1f753c03676f3d705cf4795
rikner <rikner@web.de>
2024-08-29 11:28:34 +0200

    add performance tests and baseline

--- Tests/SwiftSecretRabbitCodeTests/SwiftSecretRabbitCodeTests.swift
+++ Tests/SwiftSecretRabbitCodeTests/SwiftSecretRabbitCodeTests.swift
@@ -5,6 +5,9 @@ import XCTest
 final class SecretRabbitCodeTests: XCTestCase {
     func testSampleRateConversion() {
         let src = SecretRabbitCode()
+        guard let src else {
+            return XCTFail("Failed to initialize SecretRabbitCode")
+        }
         let input: [Float] = [0.0, 1.0, 0.0, -1.0, 0.0]
         let output = src.convertSampleRate(input: input, fromRate: 44100, toRate: 48000)
         XCTAssertNotNil(output)
@@ -13,8 +16,24 @@ final class SecretRabbitCodeTests: XCTestCase {
 
     func testSampleRateConversionError() {
         let src = SecretRabbitCode()
+        guard let src else {
+            return XCTFail("Failed to initialize SecretRabbitCode")
+        }
         let input: [Float] = [0.0, 1.0, 0.0, -1.0, 0.0]
         let output = src.convertSampleRate(input: input, fromRate: 44100, toRate: 0)
         XCTAssertNil(output)
+    }
+
+    func testPerformance() {
+        let src = SecretRabbitCode()
+        guard let src else {
+            return XCTFail("Failed to initialize SecretRabbitCode")
+        }
+        let input: [Float] = [0.0, 1.0, 0.0, -1.0, 0.0]
+        measure {
+            for _ in 0..<1000 {
+                let _ = src.convertSampleRate(input: input, fromRate: 44100, toRate: 48000)
+            }
+        }
     }
 }
