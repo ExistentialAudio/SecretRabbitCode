342b4d9b3fec88b1899afec2ae32e2c4a3209846
rikner <rikner@web.de>
2024-08-29 10:46:54 +0200

    add tests

---
+++ Tests/SwiftSecretRabbitCodeTests/SwiftSecretRabbitCodeTests.swift
@@ -0,0 +1,20 @@
+import XCTest
+
+@testable import SecretRabbitCode
+
+final class SecretRabbitCodeTests: XCTestCase {
+    func testSampleRateConversion() {
+        let src = SecretRabbitCode()
+        let input: [Float] = [0.0, 1.0, 0.0, -1.0, 0.0]
+        let output = src.convertSampleRate(input: input, fromRate: 44100, toRate: 48000)
+        XCTAssertNotNil(output)
+        XCTAssertEqual(output?.count, input.count * 48000 / 44100)
+    }
+
+    func testSampleRateConversionError() {
+        let src = SecretRabbitCode()
+        let input: [Float] = [0.0, 1.0, 0.0, -1.0, 0.0]
+        let output = src.convertSampleRate(input: input, fromRate: 44100, toRate: 0)
+        XCTAssertNil(output)
+    }
+}
