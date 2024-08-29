6fedc6feec379286026ae1b1b1a25defc2f3cfbb
rikner <rikner@web.de>
2024-08-29 14:34:54 +0200

    improve API

--- Sources/SecretRabbitCode/SecretRabbitCode.swift
+++ Sources/SecretRabbitCode/SecretRabbitCode.swift
@@ -1,17 +1,30 @@
 import CSamplerate
 
+
+enum SecretRabbitCodeError: Error {
+    case InitalizationFailed(description: String)
+    case ConversionFailed(description: String)
+}
+
+
+extension String {
+    init(from errorCode: Int32) {
+        self = String(cString: src_strerror(errorCode))
+    }
+}
+
+
 public class SecretRabbitCode {
     private var converter: OpaquePointer?
     private var error: Int32 = 0
 
-    public init?(
+    public init(
         converterType: Int32 = Int32(SRC_LINEAR),
         channels: Int32 = 1
-    ) {
+    ) throws {
         self.converter = src_new(converterType, channels, &error)
         guard error == 0 else {
-            SecretRabbitCode.printError(error)
-            return nil
+            throw SecretRabbitCodeError.InitalizationFailed(description: String(from: error))
         }
     }
 
@@ -19,19 +32,23 @@ public class SecretRabbitCode {
         src_delete(converter)
     }
 
-    public func convertSampleRate(input: [Float], fromRate: Int, toRate: Int) -> [Float]? {
-        var data_out = [Float](repeating: 0, count: input.count * toRate / fromRate)
+    public func convertSampleRate(
+        of input: [Float],
+        from inputRate: Int,
+        to outputRate: Int
+    ) throws -> [Float] {
+        var data_out = [Float](repeating: 0, count: input.count * outputRate / inputRate)
         var data_in = input.withUnsafeBufferPointer { buffer_in in
             return data_out.withUnsafeMutableBufferPointer { buffer_out in
                 return SRC_DATA(
                     data_in: buffer_in.baseAddress,
                     data_out: buffer_out.baseAddress,
                     input_frames: input.count,
-                    output_frames: input.count * toRate / fromRate,
+                    output_frames: input.count * outputRate / inputRate,
                     input_frames_used: 0,
                     output_frames_gen: 0,
                     end_of_input: 0,
-                    src_ratio: Double(toRate) / Double(fromRate)
+                    src_ratio: Double(outputRate) / Double(inputRate)
                 )
             }
         }
@@ -39,15 +56,9 @@ public class SecretRabbitCode {
         error = src_process(converter, &data_in)
 
         guard error == 0 else {
-            SecretRabbitCode.printError(error)
-            return nil
+            throw SecretRabbitCodeError.ConversionFailed(description: String(from: error))
         }
 
         return data_out
-    }
-
-    static func printError(_ error: Int32) {
-        let errorString = String(cString: src_strerror(error))
-        print("SRC error: \(errorString)")
     }
 }
--- Tests/SwiftSecretRabbitCodeTests/SwiftSecretRabbitCodeTests.swift
+++ Tests/SwiftSecretRabbitCodeTests/SwiftSecretRabbitCodeTests.swift
@@ -3,36 +3,25 @@ import XCTest
 @testable import SecretRabbitCode
 
 final class SecretRabbitCodeTests: XCTestCase {
-    func testSampleRateConversion() {
-        let src = SecretRabbitCode()
-        guard let src else {
-            return XCTFail("Failed to initialize SecretRabbitCode")
-        }
+    func testSampleRateConversion() throws {
+        let src = try SecretRabbitCode()
         let input: [Float] = [0.0, 1.0, 0.0, -1.0, 0.0]
-        let output = src.convertSampleRate(input: input, fromRate: 44100, toRate: 48000)
-        XCTAssertNotNil(output)
-        XCTAssertEqual(output?.count, input.count * 48000 / 44100)
+        let output = try src.convertSampleRate(of: input, from: 44100, to: 48000)
+        XCTAssertEqual(output.count, input.count * 48000 / 44100)
     }
 
-    func testSampleRateConversionError() {
-        let src = SecretRabbitCode()
-        guard let src else {
-            return XCTFail("Failed to initialize SecretRabbitCode")
-        }
+    func testSampleRateConversionError() throws {
+        let src = try SecretRabbitCode()
         let input: [Float] = [0.0, 1.0, 0.0, -1.0, 0.0]
-        let output = src.convertSampleRate(input: input, fromRate: 44100, toRate: 0)
-        XCTAssertNil(output)
+        XCTAssertThrowsError(try src.convertSampleRate(of: input, from: 44100, to: 0))
     }
 
-    func testPerformance() {
-        let src = SecretRabbitCode()
-        guard let src else {
-            return XCTFail("Failed to initialize SecretRabbitCode")
-        }
+    func testPerformance() throws {
+        let src = try SecretRabbitCode()
         let input: [Float] = [0.0, 1.0, 0.0, -1.0, 0.0]
         measure {
-            for _ in 0..<1000 {
-                let _ = src.convertSampleRate(input: input, fromRate: 44100, toRate: 48000)
+            for _ in 0 ..< 1000 {
+                let _ = try! src.convertSampleRate(of: input, from: 44100, to: 48000)
             }
         }
     }
